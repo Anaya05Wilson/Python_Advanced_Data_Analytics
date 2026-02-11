0277c1e26df63d485ea8d525a16737b8de0edecc
Anaya05Wilson <anayawil05@gmail.com>
2026-02-11 11:54:07 +0530

    Management program

---
+++ Placement/names.py
@@ -0,0 +1,16 @@
+class PythonDataAnalytics:
+
+    def friend1(self):
+        print("achu")
+
+    def friend2(self):
+        print("niru")
+
+    def friend3(self):
+        print("jenna")
+
+    def friend4(self):
+        print("sudu")
+
+    def friend5(self):
+        print("nandu")
